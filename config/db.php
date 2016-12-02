bb9fc9f7ee3e86f71eb8fc40b486e4a50bd47c26
pedro lemus <pedro.lemus@hotmail.es>
2016-12-02 18:22:49 +0100

    Primer commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=prueba3progra4',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
