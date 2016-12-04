b2ed8491194e3754ffeda90262b6e3f719548287
pedro lemus <pedro.lemus@hotmail.es>
2016-12-04 14:55:10 +0100

    Punto 6, funcionando pero sin usar VIEWS. Se modifico controladores de  puntuaciontbl y recetastbl.

--- controllers/PuntuaciontblController.php
+++ controllers/PuntuaciontblController.php
@@ -12,13 +12,12 @@ use yii\filters\VerbFilter;
 /**
  * PuntuaciontblController implements the CRUD actions for Puntuaciontbl model.
  */
-class PuntuaciontblController extends Controller
-{
+class PuntuaciontblController extends Controller {
+
     /**
      * @inheritdoc
      */
-    public function behaviors()
-    {
+    public function behaviors() {
         return [
             'verbs' => [
                 'class' => VerbFilter::className(),
@@ -33,14 +32,13 @@ class PuntuaciontblController extends Controller
      * Lists all Puntuaciontbl models.
      * @return mixed
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $searchModel = new PuntuaciontblSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -49,10 +47,9 @@ class PuntuaciontblController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         return $this->render('view', [
-            'model' => $this->findModel($id),
+                    'model' => $this->findModel($id),
         ]);
     }
 
@@ -61,15 +58,14 @@ class PuntuaciontblController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
+    public function actionCreate() {
         $model = new Puntuaciontbl();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -80,15 +76,14 @@ class PuntuaciontblController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -99,12 +94,98 @@ class PuntuaciontblController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
     }
+
+    /**
+     * Metodo se encarga de responder a la consulta de la url "http://localhost/prueba3hweb/web/index.php?r=puntuaciontbl/masestrellas"
+     * 
+     * @return type
+     */
+    public function actionMasestrellas() { //MasEstrellas()
+        $cocineros = \app\models\Usuariostbl::find()->all();
+        $puntuaciones = Puntuaciontbl::find()->all();
+        $index = 0;
+        foreach ($cocineros as $cocinero) {
+            $index;
+            $acumulado = 0;
+            $myArray[$index][1] = $cocinero;
+            foreach ($puntuaciones as $puntuacion) {
+
+                if ($puntuacion->recetastbl->usuariostbl->id == $cocinero->id) {
+
+                    $acumulado += $puntuacion->valoracion;
+                }
+            }
+            $myArray[$index][2] = $acumulado;
+            
+            $index++;
+        }
+        //arsort($myArray);
+        //array_multisort($myArray, SORT_NUMERIC,SORT_DESC);
+        $cont = 0;
+        $cont1 = 0;
+        $cont2 = 1;
+        $ultimoMayor = -2;
+        $primeraVuelta = true;
+        echo '<link href="/prueba3hweb/web/assets/7668a6c7/css/bootstrap.css" rel="stylesheet">';
+        echo '<script src="/prueba3hweb/web/assets/ec8baf58/jquery.js"></script><script src="/prueba3hweb/web/assets/88c8d4fd/yii.js"></script><script src="/prueba3hweb/web/assets/7668a6c7/js/bootstrap.js"></script>';
+        echo '<div class="container">
+  <h2>Cocineros con más estrellas acumuladas</h2>
+  </br>           
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Lugar</th>
+        <th>Cocinero</th>
+        <th>Total Estrellas</th>
+      </tr>
+    </thead>
+    <tbody>';
+
+        //Calcula e imprime en orden de mayor a menor estrellas acumuladas
+        while ($cont1 < $index) {
+            $mayor = -1;
+            while ($cont < $index) {
+                if (($myArray[$cont][2] > $mayor && $ultimoMayor > $myArray[$cont][2]) || ($primeraVuelta && $myArray[$cont][2] > $mayor)) {
+                    $mayor = $myArray[$cont][2];
+                    $mayorAr[0] = $myArray[$cont][1];
+                    $mayorAr[1] = $myArray[$cont][2];
+                } else {
+                    if ($ultimoMayor == 0 && $myArray[$cont][2] == 0) {
+                        $mayor = $myArray[$cont][2];
+                        $mayorAr[0] = $myArray[$cont][1];
+                        $mayorAr[1] = $myArray[$cont][2];
+                    }
+                }
+                if ($cont == $index - 1) {
+
+                    echo '<tr>
+                            <td>' . $cont2 . '</td>
+                            <td>' . $mayorAr[0]->nombre . ' '.$mayorAr[0]->apellido.'</td>
+                            <td>' . $mayorAr[1] . '</td>
+                        </tr>';
+                    $cont2++;
+                    $ultimoMayor = $mayorAr[1];
+                }
+
+                $cont++;
+            }
+            $primeraVuelta = false;
+            $cont = 0;
+            $cont1++;
+        }
+        echo '</tbody></table></div>';
+
+        //Este return lo puse solo para relleno, no se que retornar aun
+        //return $this->redirect(['index']);
+    }
+    
+    
+    
 
     /**
      * Finds the Puntuaciontbl model based on its primary key value.
@@ -113,12 +194,12 @@ class PuntuaciontblController extends Controller
      * @return Puntuaciontbl the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
+    protected function findModel($id) {
         if (($model = Puntuaciontbl::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
+
 }
--- controllers/RecetastblController.php
+++ controllers/RecetastblController.php
@@ -105,6 +105,184 @@ class RecetastblController extends Controller
 
         return $this->redirect(['index']);
     }
+    
+    /**
+     * Responde a la url "http://localhost/prueba3hweb/web/index.php?r=recetastbl/masrecetas"
+     * Ranking de los cocineros con mas recetas.
+     */
+    public function actionMasrecetas() { 
+        $cocineros = \app\models\Usuariostbl::find()->all();
+        $recetas = Recetastbl::find()->all();
+        $index = 0;
+        foreach ($cocineros as $cocinero) {
+            $index;
+            $cuentaRecetas = 0;
+            $myArray[$index][1] = $cocinero;
+            foreach ($recetas as $receta) {
+
+                if ($receta->usuariostbl_id == $cocinero->id) {
+
+                    $cuentaRecetas++;
+                }
+            }
+            $myArray[$index][2] = $cuentaRecetas;
+            $index++;
+        }
+
+        $cont = 0;
+        $cont1 = 0;
+        $cont2 = 1;
+        $ultimoMayor = -2;
+        $primeraVuelta = true;
+        echo '<link href="/prueba3hweb/web/assets/7668a6c7/css/bootstrap.css" rel="stylesheet">';
+        echo '<script src="/prueba3hweb/web/assets/ec8baf58/jquery.js"></script><script src="/prueba3hweb/web/assets/88c8d4fd/yii.js"></script><script src="/prueba3hweb/web/assets/7668a6c7/js/bootstrap.js"></script>';
+        echo '<div class="container">
+  <h2>Cocineros con más recetas</h2>
+  </br>           
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Lugar</th>
+        <th>Cocinero</th>
+        <th>Total Recetas</th>
+      </tr>
+    </thead>
+    <tbody>';
+
+        //Calcula e imprime en orden de mayor a menor cantidad de recetas
+        while ($cont1 < $index) {
+            $mayor = -1;
+            while ($cont < $index) {
+                if (($myArray[$cont][2] > $mayor && $ultimoMayor > $myArray[$cont][2]) || ($primeraVuelta && $myArray[$cont][2] > $mayor)) {
+                    $mayor = $myArray[$cont][2];
+                    $mayorAr[0] = $myArray[$cont][1];
+                    $mayorAr[1] = $myArray[$cont][2];
+                } else {
+                    if ($ultimoMayor == 0 && $myArray[$cont][2] == 0) {
+                        $mayor = $myArray[$cont][2];
+                        $mayorAr[0] = $myArray[$cont][1];
+                        $mayorAr[1] = $myArray[$cont][2];
+                    }
+                }
+                if ($cont == $index - 1) {
+
+                    echo '<tr>
+                            <td>' . $cont2 . '</td>
+                            <td>' . $mayorAr[0]->nombre . ' '.$mayorAr[0]->apellido.'</td>
+                            <td>' . $mayorAr[1] . '</td>
+                        </tr>';
+                    //echo $mayorAr[0]->nombre . ' : ' . $mayorAr[1];
+                    //echo ' - ';
+                    $cont2++;
+                    $ultimoMayor = $mayorAr[1];
+                }
+
+                $cont++;
+            }
+            $primeraVuelta = false;
+            $cont = 0;
+            $cont1++;
+        }
+        echo '</tbody></table></div>';
+
+        //Este return lo puse solo para relleno, no se que retornar aun
+        //return $this->redirect(['index']);
+    }
+    
+    /**
+     * Responde a la url "http://localhost/prueba3hweb/web/index.php?r=recetastbl/mpromestrellas"
+     * Ranking de los cocineros con mas recetas.
+     */
+    public function actionMpromestrellas() { 
+        $puntuaciones = \app\models\Puntuaciontbl::find()->all();
+        $recetas = Recetastbl::find()->all();
+        $index = 0;
+        foreach ($recetas as $receta) {
+            $index;
+            $cuentaRecetas = 0;
+            $valorAcumulado = 0;
+            $promedio = 0;
+            $myArray[$index][1] = $receta;
+            foreach ($puntuaciones as $puntuacion) {
+
+                if ($puntuacion->recetastbl_id == $receta->id) {
+                    $valorAcumulado += $puntuacion->valoracion;
+                    $cuentaRecetas++;
+                }
+            }
+            if($cuentaRecetas == 0)
+            {
+                $promedio = 0;
+                $myArray[$index][2] = $promedio;
+            }else
+            {
+                $promedio =(1.00*$valorAcumulado)/(1.00*$cuentaRecetas);
+                $myArray[$index][2] = $promedio;
+            }
+            
+            $index++;
+        }
+        $cont = 0;
+        $cont1 = 0;
+        $cont2 = 1;
+        $ultimoMayor = -2;
+        $primeraVuelta = true;
+        echo '<link href="/prueba3hweb/web/assets/7668a6c7/css/bootstrap.css" rel="stylesheet">';
+        echo '<script src="/prueba3hweb/web/assets/ec8baf58/jquery.js"></script><script src="/prueba3hweb/web/assets/88c8d4fd/yii.js"></script><script src="/prueba3hweb/web/assets/7668a6c7/js/bootstrap.js"></script>';
+        echo '<div class="container">
+  <h2>Recetas con mejor promedio de estrellas</h2>
+  </br>           
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Lugar</th>
+        <th>Receta</th>
+        <th>Autor</th>
+        <th>Promedio Estrellas</th>
+      </tr>
+    </thead>
+    <tbody>';
+
+        //Calcula e imprime en orden de mayor a menor promedio de estrellas
+        while ($cont1 < $index) {
+            $mayor = -1;
+            while ($cont < $index) {
+                if (($myArray[$cont][2] > $mayor && $ultimoMayor > $myArray[$cont][2]) || ($primeraVuelta && $myArray[$cont][2] > $mayor)) {
+                    $mayor = $myArray[$cont][2];
+                    $mayorAr[0] = $myArray[$cont][1];
+                    $mayorAr[1] = $myArray[$cont][2];
+                } else {
+                    if ($ultimoMayor == 0 && $myArray[$cont][2] == 0) {
+                        $mayor = $myArray[$cont][2];
+                        $mayorAr[0] = $myArray[$cont][1];
+                        $mayorAr[1] = $myArray[$cont][2];
+                    }
+                }
+                if ($cont == $index - 1) {
+
+                    echo '<tr>
+                            <td>' . $cont2 . '</td>
+                            <td>' . $mayorAr[0]->receta.'</td>
+                            <td>' . $mayorAr[0]->usuariostbl->nombre.' '.$mayorAr[0]->usuariostbl->apellido.'</td>
+                            <td>' . $mayorAr[1] . '</td>
+                        </tr>';
+                    //echo $mayorAr[0]->nombre . ' : ' . $mayorAr[1];
+                    //echo ' - ';
+                    $cont2++;
+                    $ultimoMayor = $mayorAr[1];
+                }
+
+                $cont++;
+            }
+            $primeraVuelta = false;
+            $cont = 0;
+            $cont1++;
+        }
+        echo '</tbody></table></div>';
+
+        //Este return lo puse solo para relleno, no se que retornar aun
+        //return $this->redirect(['index']);
+    }
 
     /**
      * Finds the Recetastbl model based on its primary key value.
